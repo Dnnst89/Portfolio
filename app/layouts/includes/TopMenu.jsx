1c1ce8b0aaedc5729c71925d5ff1a4121eb2f481
DannyST89 <danny@centaurosolutions.com>
2023-09-04 23:19:36 -0600

    Se crean las rutas y se agrega las validaciones en el formulario de registro

--- app/layouts/includes/TopMenu.jsx
+++ app/layouts/includes/TopMenu.jsx
@@ -5,7 +5,6 @@ import { BsCart4 } from 'react-icons/bs';
 import Search from '../../../components/Search';
 import { img2 } from '../../assets/images';
 import Image from 'next/image';
-import { BiSolidUserCircle } from 'react-icons/bi';
 import AccountDropodown from '@/components/AccountDropodown';
 const TopMenu = () => {
     return (
@@ -15,7 +14,7 @@ const TopMenu = () => {
                     <Image src={img2} alt="dfskdk" width={100} height={100} />
                 </div>
 
-                <div className="flex justify-center items-center  order-3 sm:order-2 col-span-2 sm:col-span-4 h-[60px] ">
+                <div className="flex justify-center items-center  order-3 sm:order-2 col-span-2 sm:col-span-4 h-[60px]">
                     {<Search />}
                 </div>
                 <div className="grid grid-cols-2 justify-center items-center  order-2 sm:order-3 col-span-1 sm:col-span-1 ">
---
+++ app/register/singname/page.jsx
@@ -0,0 +1,11 @@
+import RegisterFormOne from '@/components/RegisterFormOne';
+
+const SingName = () => {
+    return (
+        <div>
+            <RegisterFormOne />
+        </div>
+    );
+};
+
+export default SingName;
