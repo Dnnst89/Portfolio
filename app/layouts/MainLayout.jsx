3ebae5fed3ed8d636f29bc0db16498ae3d7c38c1
DannyST89 <danny@centaurosolutions.com>
2023-09-07 09:22:36 -0600

    se arregla el error de graph

--- app/layouts/MainLayout.jsx
+++ app/layouts/MainLayout.jsx
@@ -1,16 +1,14 @@
-'use client';
+"use client";
 
-import Product from '@/components/Product';
-import ProductContainer from '@/app/layouts/includes/ProductContainer';
-
-import ToogleSideBar from '@/components/ToogleSideBar';
-import React from 'react';
-import Navbar from './includes/Navbar';
-import TopMenu from './includes/TopMenu';
-import SideBar from './includes/SideBar';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import Navbar from "./includes/Navbar";
+import TopMenu from "./includes/TopMenu";
+import { useQuery } from "@apollo/client";
+//import GET_USERS from "@/src/graphQl/queries/getUsers";
 
 const MainLayour = ({ children }) => {
+    //const { data } = useQuery(GET_USERS);
+    // console.log(data);
     return (
         <>
             <div className="">
@@ -18,7 +16,7 @@ const MainLayour = ({ children }) => {
                 <Navbar />
             </div>
 
-            <main className="bg-pink">{children}</main>
+            <main className="">{children}</main>
 
             <div>foot</div>
         </>
--- src/graphQl/config.js
+++ src/graphQl/config.js
@@ -1,29 +1,8 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 const URL_API = process.env.URL_API;
 const client = new ApolloClient({
-    uri: URL_API,
-    cache: new InMemoryCache(),
+  uri: URL_API,
+  cache: new InMemoryCache(),
 });
 
-// import { ApolloClient, InMemoryCache } from '@apollo/client';
-// const URL_API = process.env.URL_API;
-// const client = new ApolloClient({
-//     uri: URL_API,
-//     cache: new InMemoryCache(),
-// });
-
 export default client;
-
-// export const GET_PRODUCTS = gql`
-//     query GetLocations {
-//         products {
-//             data {
-//                 id
-//                 attributes {
-//                     name
-//                 }
-//             }
-//         }
-//     }
-// `;
