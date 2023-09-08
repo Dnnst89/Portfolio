1e6a061c783ddda82ed2ccf757f8afe7579ee6fb
elias-benavides <elias.benavides@centaurosolutions.com>
2023-09-08 10:46:35 -0600

    fix dev

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,55 +1,53 @@
-'use client';
-import React from 'react';
-import { useState } from 'react';
-import algoliasearch from 'algoliasearch/lite';
-import styles from '../styles/Home.module.css';
-import ProductContainer from '@/app/layouts/includes/ProductContainer';
+"use client";
+import React from "react";
+import { useState } from "react";
+import algoliasearch from "algoliasearch/lite";
+import styles from "../styles/Home.module.css";
+import ProductContainer from "@/app/layouts/includes/ProductContainer";
 
-const id= process.env.NEXT_PUBLIC_APPLICATION_ID
-const key= process.env.NEXT_PUBLIC_SEARCH_API_KEY
-const indexAlgolia = process.env.NEXT_PUBLIC_ALGOLIA_INDEX
+const id = process.env.NEXT_PUBLIC_APPLICATION_ID;
+const key = process.env.NEXT_PUBLIC_SEARCH_API_KEY;
+const indexAlgolia = process.env.NEXT_PUBLIC_ALGOLIA_INDEX;
 
 const searchClient = algoliasearch(id, key);
 const index = searchClient.initIndex(indexAlgolia);
 
 const Search = () => {
+  const [results, setResults] = useState(null);
 
-    const [results, setResults] = useState(null);
+  const performSearch = async (value) => {
+    const { hits } = await index.search(value, {});
 
-    const performSearch = async (value) => {
-        const { hits } = await index.search(value, {});
+    const results = hits.map((hit) => {
+      const { objectID: key, href, _highlightResult } = hit;
+      const {
+        name: { value: name },
+      } = _highlightResult;
+      return { key, href, name };
+    });
 
-        const results = hits.map((hit) => {
-            const { objectID: key, href, _highlightResult } = hit;
-            const {
-                name: { value: name },
-            } = _highlightResult;
-            return { key, href, name };
-        });
+    setResults(results);
+  };
 
-        setResults(results);
-    };
+  const handleChange = (e) => {
+    const { value } = e.target;
+    value === "" ? setResults(null) : performSearch(value);
+  };
 
-    const handleChange = (e) => {
-        const { value } = e.target;
-        value === '' ? setResults(null) : performSearch(value);
-    };
-
-    return (
-        <div>
-            <form className={styles.search}>
-                <input
-                    placeholder="Busca aquí lo que quieras..."
-                    onChange={handleChange}
-                    type="search"
-                />
-            </form>
-            <div className="py-20">
-                <ProductContainer products={results} />
-            </div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <form className={styles.search}>
+        <input
+          placeholder="Busca aquí lo que quieras..."
+          onChange={handleChange}
+          type="search"
+        />
+      </form>
+      <div className="py-20">
+        <ProductContainer products={results} />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
