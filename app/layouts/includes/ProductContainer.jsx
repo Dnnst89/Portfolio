483fe59f5e78ae1eaa3f1aef6bf8fff3b7a1bddf
elias-benavides <elias.benavides@centaurosolutions.com>
2023-09-11 14:43:38 -0600

    fix dev

--- app/layouts/includes/ProductContainer.jsx
+++ app/layouts/includes/ProductContainer.jsx
@@ -3,20 +3,32 @@ import Image from "next/image";
 import Product from "../../../components/Product";
 import Pagination from "@/components/Pagination";
 
-const ProductContainer = ({ result, hitsPerPage, nbHits, nbPages, currentPage, setCurrentPage }) => {
+const ProductContainer = ({
+  result,
+  hitsPerPage,
+  nbHits,
+  nbPages,
+  currentPage,
+  setCurrentPage,
+}) => {
+  const { hits } = result;
 
-  const {hits} = result
- 
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 w-full">
         {hits
           ? hits.map((item) => {
-            return <Product id={item.id} name={item.name} />;
-          })
+              return <Product key={item.id} id={item.id} name={item.name} />;
+            })
           : null}
       </div>
-      <Pagination hitsPerPage={hitsPerPage} nbHits={nbHits} nbPages={nbPages} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+      {/* <Pagination
+        hitsPerPage={hitsPerPage}
+        nbHits={nbHits}
+        nbPages={nbPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      /> */}
     </>
   );
 };
--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -1,51 +1,69 @@
-import React from 'react'
+import React from "react";
 
-const Pagination = ({ hitsPerPage, nbHits, nbPages, currentPage, setCurrentPage }) => {
+const Pagination = ({
+  hitsPerPage,
+  nbHits,
+  nbPages,
+  currentPage,
+  setCurrentPage,
+}) => {
+  const noPages = [];
 
-    const noPages = []
+  for (let index = 1; index <= nbPages; index++) {
+    noPages.push(index);
+  }
 
-    for (let index = 1; index <= nbPages; index++) {
-        noPages.push(index)
-    }
+  const onPreviusPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
 
-    const onPreviusPage = ()=> {
-        setCurrentPage(currentPage - 1)
-    }
+  const onNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
-    const onNextPage = ()=> {
-        setCurrentPage(currentPage + 1)
-    }
+  const onSpecificPage = (number) => {
+    setCurrentPage(number);
+  };
+  console.log(noPages);
+  return (
+    <div>
+      <nav aria-label="Page navigation example">
+        <ul className="inline-flex -space-x-px text-sm">
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              onClick={onPreviusPage}
+            >
+              Anterior
+            </a>
+          </li>
+          {noPages.map((noPage) => (
+            <li>
+              <a
+                href="#"
+                className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white bg${
+                  noPage === currentPage + 1 ? "-white" : ""
+                }`}
+                onClick={() => onSpecificPage(noPage)}
+              >
+                {noPage}
+              </a>
+            </li>
+          ))}
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              onClick={onNextPage}
+            >
+              Siguiente
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-    const onSpecificPage = (number)=> {
-        setCurrentPage(number)
-    }
-
-
-    return (
-
-        <div>
-            <nav aria-label="Page navigation example">
-                <ul className="inline-flex -space-x-px text-sm">
-                    <li>
-                        <a href="#" className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={onPreviusPage}>Anterior</a>
-                    </li>
-                    {
-                        noPages.map(noPage => (
-                            <li>
-                                <a href="#" className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white bg${noPage===currentPage+1 ? '-white' : "" }`} onClick={()=>onSpecificPage(noPage)}>
-                                    {noPage}
-                                </a>
-                            </li>
-                        ))
-                    }
-                    <li>
-                        <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={onNextPage}>Siguiente</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-
-    )
-}
-
-export default Pagination
+export default Pagination;
