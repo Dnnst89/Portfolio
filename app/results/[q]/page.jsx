08a1e01424460f4a63c0a87cdb425d15a86c827c
elias-benavides <elias.benavides@centaurosolutions.com>
2023-09-11 14:53:48 -0600

    fix app

--- app/results/[q]/page.jsx
+++ app/results/[q]/page.jsx
@@ -1,58 +1,74 @@
-'use client';
+"use client";
 import { useEffect, useState } from "react";
 import Navbar from "@/app/layouts/includes/Navbar";
 import TopMenu from "@/app/layouts/includes/TopMenu";
 import ProductContainer from "@/app/layouts/includes/ProductContainer";
 
 export default function GetResults({ params }) {
-    const [result, setResult] = useState([]);
-    const [hitsPerPage, setHitsPerPage] = useState(null);
-    const [nbHits, setNbHits] = useState(null);
-    const [nbPages, setNbPages] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0);
-    
-    const { q } = params
+  const [result, setResult] = useState([]);
+  const [hitsPerPage, setHitsPerPage] = useState(null);
+  const [nbHits, setNbHits] = useState(null);
+  const [nbPages, setNbPages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
-    async function getHits() {
-        try {
-            const response = await fetch(`https://6TQCC8J5LB.algolia.net/1/indexes/development_api::product.product?query=${q}&page=${currentPage}`, { headers: { "X-Algolia-Api-Key": "5a6490a15e1b2c9a3c53d7f8328c3f8d", "X-Algolia-Application-Id": "6TQCC8J5LB" } });
+  const { q } = params;
 
-            if (!response.ok) {
-                throw new Error(`Error! status: ${response.status}`);
-            }
-            const  hits  = await response.json();
-            return hits;
-        } catch (err) {
-            console.log(err);
+  async function getHits() {
+    try {
+      const response = await fetch(
+        `https://6TQCC8J5LB.algolia.net/1/indexes/development_api::product.product?query=${q}&page=${currentPage}`,
+        {
+          headers: {
+            "X-Algolia-Api-Key": "5a6490a15e1b2c9a3c53d7f8328c3f8d",
+            "X-Algolia-Application-Id": "6TQCC8J5LB",
+          },
         }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+      const hits = await response.json();
+      return hits;
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    async function allResults() {
-        const result = await getHits()
-        const {hitsPerPage} = result
-        const {nbHits} = result
-        const {nbPages} = result
-        setResult(result)
-        setHitsPerPage(hitsPerPage)
-        setNbHits(nbHits)
-        setNbPages(nbPages)
-        
-    }
+  async function allResults() {
+    const result = await getHits();
+    const { hitsPerPage } = result;
+    const { nbHits } = result;
+    const { nbPages } = result;
+    setResult(result);
+    setHitsPerPage(hitsPerPage);
+    setNbHits(nbHits);
+    setNbPages(nbPages);
+  }
 
-    useEffect(() => {
-        if (!q) return null;
-        allResults()
-    }, [currentPage]);
+  useEffect(() => {
+    if (!q) return null;
+    allResults();
 
-    console.log(result)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
 
-    return (
-        <>
-            <div className="">
-                <TopMenu />
-                <Navbar />
-            </div>
-            <ProductContainer result={result} hitsPerPage={hitsPerPage} nbHits={nbHits} nbPages={nbPages} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
-        </>
-    )
+  console.log(result);
+
+  return (
+    <>
+      <div className="">
+        <TopMenu />
+        <Navbar />
+      </div>
+      <ProductContainer
+        result={result}
+        hitsPerPage={hitsPerPage}
+        nbHits={nbHits}
+        nbPages={nbPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
+  );
 }
--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -38,8 +38,8 @@ const Pagination = ({
               Anterior
             </a>
           </li>
-          {noPages.map((noPage) => (
-            <li>
+          {noPages.map((noPage, idx) => (
+            <li key={idx}>
               <a
                 href="#"
                 className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white bg${
