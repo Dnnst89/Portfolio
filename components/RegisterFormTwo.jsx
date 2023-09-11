93991bd7293840bea790d941bb67549860592c96
elias-benavides <elias.benavides@centaurosolutions.com>
2023-09-11 09:31:08 -0600

    add react-hot-toast and add alert in form

--- components/RegisterFormTwo.jsx
+++ components/RegisterFormTwo.jsx
@@ -11,6 +11,7 @@ import RegisterUser from "@/src/graphQl/queries/registerUser";
 import { createElement } from "@/src/store/store";
 import { useRouter } from "next/navigation";
 import Spinner from "./Spinner";
+import { Toaster, toast } from "react-hot-toast";
 
 const initialValues = {
   email: "",
@@ -40,7 +41,6 @@ const RegisterFormTwo = () => {
   const { username } = useSelector((x) => x.registryForm);
   const router = useRouter();
   const [createUser] = useMutation(RegisterUser);
-
   const handleSubmit = async (values) => {
     const dataValues = Object.keys(values).map((el) => {
       return values[el];
@@ -55,14 +55,14 @@ const RegisterFormTwo = () => {
       const { data } = await createUser({
         variables: { username, email, password },
       });
-      if (!data) {
-        throw new Error("Algo ha ocurrido");
-      }
-      createElement("userData", JSON.stringify(data.register));
       router.push("/");
-      setLoading(false);
+      createElement("userData", JSON.stringify(data.register));
     } catch (error) {
-      console.log(error);
+      toast.error(
+        "No se pudo registrar tu cuenta, por favor intentalo más tarde"
+      );
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -76,61 +76,73 @@ const RegisterFormTwo = () => {
         >
           {({ errors, touched }) => {
             return (
-              <Form>
-                <h2 className="text-orange font-semibold flex justify-center items-center h-[50px] ">
-                  Verifica tu cuenta
-                </h2>
-                <div>
-                  <Field
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Correo Electrónico"
-                    className="focus:border-blue-500 outline-none w-full px-6 py-2 mb-2 border  rounded-lg border-none"
-                    autoFocus={true}
-                  />
-                  {errors.email && touched.email ? (
-                    <ErrorForm>{errors.email}</ErrorForm>
-                  ) : null}
-                </div>
-                <div>
-                  <Field
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Constraseña"
-                    className="focus:border-blue-500 outline-none w-full px-6 py-2 mb-2 border  rounded-lg border-none"
-                    autoFocus={true}
-                  />
-                  {errors.password && touched.password ? (
-                    <ErrorForm>{errors.password}</ErrorForm>
-                  ) : null}
-                </div>
-                <div>
-                  <Field
-                    type="password"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    placeholder="Confirmar contraseña"
-                    className="focus:border-blue-500 outline-none w-full px-6 py-2 mb-2 border  rounded-lg border-none"
-                    autoFocus={true}
-                  />
-                  {errors.confirmPassword && touched.confirmPassword ? (
-                    <ErrorForm>{errors.confirmPassword}</ErrorForm>
-                  ) : null}
-                </div>
-                <button
-                  type="submit"
-                  className="bg-blue-500 hover:bg-blue-300 text-whitetext-base rounded-lg py-2 px-5 transition-colors w-full text-[19px] text-white bg-aquamarine disabled:opacity-50"
-                  disabled={
-                    (Object.keys(errors).length &&
-                      Object.keys(touched).length) ||
-                    loading
-                  }
-                >
-                  {loading ? <Spinner /> : "Registrame"}
-                </button>
-              </Form>
+              <>
+                <Form>
+                  <h2 className="text-orange font-semibold flex justify-center items-center h-[50px] ">
+                    Verifica tu cuenta
+                  </h2>
+
+                  <div>
+                    <Field
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="Correo Electrónico"
+                      className="focus:border-blue-500 outline-none w-full px-6 py-2 mb-2 border  rounded-lg border-none"
+                      autoFocus={true}
+                    />
+                    {errors.email && touched.email ? (
+                      <ErrorForm>{errors.email}</ErrorForm>
+                    ) : null}
+                  </div>
+                  <div>
+                    <Field
+                      type="password"
+                      id="password"
+                      name="password"
+                      placeholder="Constraseña"
+                      className="focus:border-blue-500 outline-none w-full px-6 py-2 mb-2 border  rounded-lg border-none"
+                      autoFocus={true}
+                    />
+                    {errors.password && touched.password ? (
+                      <ErrorForm>{errors.password}</ErrorForm>
+                    ) : null}
+                  </div>
+                  <div>
+                    <Field
+                      type="password"
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      placeholder="Confirmar contraseña"
+                      className="focus:border-blue-500 outline-none w-full px-6 py-2 mb-2 border  rounded-lg border-none"
+                      autoFocus={true}
+                    />
+                    {errors.confirmPassword && touched.confirmPassword ? (
+                      <ErrorForm>{errors.confirmPassword}</ErrorForm>
+                    ) : null}
+                  </div>
+                  <button
+                    type="submit"
+                    className="bg-blue-500 hover:bg-blue-300 text-whitetext-base rounded-lg py-2 px-5 transition-colors w-full text-[19px] text-white bg-aquamarine disabled:opacity-50"
+                    disabled={
+                      (Object.keys(errors).length &&
+                        Object.keys(touched).length) ||
+                      loading
+                    }
+                  >
+                    {loading ? <Spinner /> : "Registrame"}
+                  </button>
+                </Form>
+                <Toaster
+                  toastOptions={{
+                    style: {
+                      backgroundColor: "#be123d",
+                      color: "#FFF",
+                      fontSize: "14px",
+                    },
+                  }}
+                />
+              </>
             );
           }}
         </Formik>
--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -6,7 +6,7 @@ const Spinner = () => {
       <svg
         aria-hidden="true"
         role="status"
-        class="inline w-5 h-5 mr-3 text-white animate-spin"
+        className="inline w-5 h-5 mr-3 text-white animate-spin"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
