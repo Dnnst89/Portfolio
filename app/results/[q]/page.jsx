fff771918b761c18639f8c7d9ffc4b5eafcbe249
elias-benavides <elias.benavides@centaurosolutions.com>
2023-09-14 08:58:37 -0600

    fix dev

--- app/results/[q]/page.jsx
+++ app/results/[q]/page.jsx
@@ -45,6 +45,7 @@ export default function GetResults({ params }) {
   useEffect(() => {
     if (!q) return null;
     allResults();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentPage]);
 
   return (
