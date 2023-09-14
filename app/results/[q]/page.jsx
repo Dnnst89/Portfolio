173879c008139a32d14e9874f34c984faec19bce
elias-benavides <elias.benavides@centaurosolutions.com>
2023-09-14 10:22:29 -0600

    save changes

--- app/results/[q]/page.jsx
+++ app/results/[q]/page.jsx
@@ -1,63 +1,23 @@
-"use client";
-import { useEffect, useState } from "react";
-import ProductContainer from "@/app/layouts/includes/ProductContainer";
+import { algoliaInstace } from "@/src/axios/algoliaIntance/config";
+import ResultsComponent from "@/components/ResultsComponent";
 
 export default function GetResults({ params }) {
-  const [result, setResult] = useState([]);
-  const [hitsPerPage, setHitsPerPage] = useState(null);
-  const [nbHits, setNbHits] = useState(null);
-  const [nbPages, setNbPages] = useState([]);
-  const [currentPage, setCurrentPage] = useState(0);
-
-  const { q } = params;
-
-  async function getHits() {
-    try {
-      const response = await fetch(
-        `https://6TQCC8J5LB.algolia.net/1/indexes/development_api::product.product?query=${q}&page=${currentPage}`,
-        {
-          headers: {
-            "X-Algolia-Api-Key": "5a6490a15e1b2c9a3c53d7f8328c3f8d",
-            "X-Algolia-Application-Id": "6TQCC8J5LB",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-      const hits = await response.json();
-      return hits;
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  async function allResults() {
-    const result = await getHits();
-    const { hitsPerPage, nbHits, nbPages } = result;
-    setResult(result);
-    setHitsPerPage(hitsPerPage);
-    setNbHits(nbHits);
-    setNbPages(nbPages);
-  }
-
-  useEffect(() => {
-    if (!q) return null;
-    allResults();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage]);
-
   return (
     <>
-      <ProductContainer
-        result={result}
-        hitsPerPage={hitsPerPage}
-        nbHits={nbHits}
-        nbPages={nbPages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+      <ResultsComponent query={params.q} />
     </>
   );
+}
+
+export async function getStaticPaths() {
+  const { data } = await algoliaInstace.get(
+    `/development_api::product.product/`
+  );
+
+  const paths = data?.hits.map((product) => ({
+    params: { q: product.name.toString() },
+  }));
+  console.log(paths);
+
+  return { paths, fallback: true };
 }
---
+++ components/ResultsComponent.jsx
@@ -0,0 +1,56 @@
+"use client";
+import ProductContainer from "@/app/layouts/includes/ProductContainer";
+import { algoliaInstace } from "@/src/axios/algoliaIntance/config";
+import React, { useEffect, useState } from "react";
+
+const ResultsComponent = ({ query }) => {
+  const [result, setResult] = useState([]);
+  const [hitsPerPage, setHitsPerPage] = useState(null);
+  const [nbHits, setNbHits] = useState(null);
+  const [nbPages, setNbPages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  async function getHits() {
+    try {
+      const { data, statusText, status } = await algoliaInstace.get(
+        `/development_api::product.product?query=${query}&page=${currentPage}`
+      );
+      if (statusText !== "OK") {
+        throw new Error(`Error! status: ${status}`);
+      }
+      return data.hits;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function allResults() {
+    const result = await getHits();
+    console.log(result);
+    const { hitsPerPage, nbHits, nbPages } = result;
+    console.log(hitsPerPage, nbHits, nbPages);
+    setResult(result);
+    setHitsPerPage(hitsPerPage);
+    setNbHits(nbHits);
+    setNbPages(nbPages);
+  }
+
+  useEffect(() => {
+    if (!query) return null;
+    allResults();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage, query]);
+
+  return (
+    <ProductContainer
+      result={result}
+      hitsPerPage={hitsPerPage}
+      nbHits={nbHits}
+      nbPages={nbPages}
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+    />
+  );
+};
+
+export default ResultsComponent;
