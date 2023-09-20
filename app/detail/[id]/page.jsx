8738d6df42c90db9951209b524e8f1b5fb20afb6
danii230 <daniela.lopez@centaurosolutions.com>
2023-09-19 19:21:56 -0600

    Show Reviews

--- app/detail/[id]/page.jsx
+++ app/detail/[id]/page.jsx
@@ -5,43 +5,49 @@ import RelatedItems from "@/components/RelatedItems";
 import { atRule } from "postcss";
 import { Convergence } from "next/font/google";
 
-async function getData(id) {
+const getData = (id) => {
   try {
-    const res = await fetch(
-      `http://ec2-54-189-90-96.us-west-2.compute.amazonaws.com:1337/api/products/${id}?populate=*`
-    );
-    const { data } = await res.json();
-    return data;
+    return fetch(
+      `http://ec2-54-189-90-96.us-west-2.compute.amazonaws.com:1337/api/products/${id}?populate=*`, {
+      next: {
+        revalidate: 60
+      }
+    })
+      .then(res => res.json())
+
   } catch (error) {
     console.log(error);
   }
 }
-
 export default async function Post({ params }) {
   const { id } = params;
 
-  const {attributes} = await getData(id);
+  const { data } = await getData(id);
+  const { attributes } = data
+
+  const reviews = attributes.reviews.data
+
   const coverImage = attributes.coverImage.data
 
   const materials = attributes.materials.data
   let one = materials[0];
-  const material= one.attributes.name
+  const material = one.attributes.name
 
   const variants = attributes.variants.data
   let two = variants[0];
-  const variant= two.attributes
+  const variant = two.attributes
 
-  
+
   const categories = attributes.categories.data
   let three = categories[0];
-  const category= three.attributes.name
+  const category = three.attributes.name
 
   const { name, description, defaultPrice, sku } = attributes;
 
   return (
     <main>
-      <ProductDetail name={name} description={description} defaultPrice={defaultPrice} sku={sku} coverImage={coverImage} material={material} variant={variant} category={category}/>
-      <ProductDetailTable description={description} />
+      <ProductDetail name={name} description={description} defaultPrice={defaultPrice} sku={sku} coverImage={coverImage} material={material} variant={variant} category={category} />
+      <ProductDetailTable description={description} reviews={reviews} />
       <RelatedItems />
     </main>
   );
