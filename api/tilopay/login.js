2ad6833e61f65e186269d61f39b4dc937f4d240d
DannyST89 <danny@centaurosolutions.com>
2023-09-23 13:26:06 -0600

    fix method undefined

--- api/tilopay/login.js
+++ api/tilopay/login.js
@@ -1,10 +1,6 @@
-const API_USER = process.env.NEXT_PUBLIC_TILOPAY_API_USER;
-const API_PASSWORD = process.env.NEXT_PUBLIC_TILOPAY_API_PASSWORD;
-const API_URL = process.env.NEXT_PUBLIC_TILOPAY_LOGIN; // Replace with your API endpoint
-
 let accessToken = null;
 let expirationTimestamp = null;
-
+let type_token = null;
 export const login = async () => {
   try {
     if (!accessToken || isAccessTokenExpired()) {
@@ -27,26 +23,26 @@ export const isAccessTokenExpired = () => {
 export async function refreshToken() {
   try {
     // Send credentials and request a new token
-    const response = await fetch(API_URL, {
+    const response = await fetch(process.env.NEXT_PUBLIC_TILOPAY_LOGIN, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
+
       body: JSON.stringify({
-        apiuser: API_USER,
-        password: API_PASSWORD,
+        apiuser: process.env.NEXT_PUBLIC_TILOPAY_API_USER,
+        password: process.env.NEXT_PUBLIC_TILOPAY_API_PASSWORD,
       }),
     });
-
     // Check if the response is correct
     if (response.status === 200) {
       const accessData = await response.json();
       accessToken = accessData.access_token;
-      // Calculate the expiration timestamp based on 'expires_in' (assuming it's in seconds)
       expirationTimestamp = Date.now() + accessData.expires_in * 1000; // Convert seconds to milliseconds
+      type_token = accessData.token_type;
       return accessToken;
     } else {
-      console.error("An error occurred:", response.data.error);
+      console.error("An error occurred:", response.status, response.statusText);
       throw new Error("Failed to obtain access token.");
     }
   } catch (error) {
--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,22 +1,66 @@
-"use client";
-import "../../styles/checkout.page.css";
+import { login } from "@/api/tilopay/login";
 import CheckOutForm1 from "@/components/CheckOutForm1";
 import CheckOutForm2 from "@/components/CheckOutForm2";
 import CheckOutForm3 from "@/components/CheckOutForm3";
 import CheckOutHeader from "@/components/CheckoutHeader";
-import { login } from "../../api/tilopay/login";
-export default function CheckOut() {
-  const accessCredential = login();
-  console.log("from checkout ", accessCredential);
+import { paymentDataForm } from "../data/tilopay/transactionData";
+const PAYMENT_URL = process.env.NEXT_PUBLIC_TILOPAY_PROCESS_TO_PAYMENT;
+
+const CheckOut = async () => {
+  const token = await login();
+
+  // object structured based on the response
+  let payment_response = {
+    type: "",
+    html: "",
+    url: "",
+  };
+
   /*
-    -Login to make the call Tilopay and make the payment
+    - Login to make the call to Tilopay and make the payment
   */
-  return (
-    <div>
-      <CheckOutHeader />
-      <CheckOutForm1 />
-      <CheckOutForm2 />
-      <CheckOutForm3 />
-    </div>
-  );
-}
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+
+  try {
+    const response = await fetch(PAYMENT_URL, {
+      method: "POST",
+      headers,
+      body: paymentDataForm, // Ensure `transactionData` is defined somewhere
+    });
+
+    console.log("Response:", response);
+
+    if (response.status === 200) {
+      const paymentData = await response.json();
+      console.log("Payment Data:", paymentData);
+
+      payment_response = paymentData.url;
+
+      // Redirect to the payment URL
+      return (
+        <div>
+          <CheckOutHeader />
+          <CheckOutForm1 />
+          <CheckOutForm2 />
+          <CheckOutForm3 payment_response={payment_response.url} />
+        </div>
+      );
+    } else {
+      console.error("An error occurred:", response);
+
+      // Log the response text for more information
+      const errorText = await response.text();
+      console.error("Response Text:", errorText);
+
+      throw new Error("Failed to make the payment.");
+    }
+  } catch (error) {
+    console.error("Error requesting access data:", error);
+  }
+};
+
+export default CheckOut;
