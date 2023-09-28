0c414f5e2bcfe3eadf5119966d870b9cd8d06644
elias-benavides <elias.benavides@centaurosolutions.com>
2023-09-28 13:54:27 -0600

    fix dev

--- middleware.js
+++ middleware.js
@@ -7,17 +7,28 @@ export async function middleware(request) {
       cookies,
     } = request;
     const cookie = cookies.get("userData");
-    const userData = JSON.parse(cookie.value);
+    const userData = JSON.parse(cookie?.value);
+    if(!userData) {
+      if(
+        pathname === "/register/signemail" ||
+        pathname === "/register/singname" || 
+        pathname === "/login"
+      ) {
+        return NextResponse.next();
+      }
+    }
     if (userData && userData !== "null" && userData.user?.id) {
       // check if the user is logued
       if (
         pathname === "/register/signemail" ||
-        pathname === "/register/singname"
+        pathname === "/register/singname" || 
+        pathname === "/login"
       ) {
         return NextResponse.redirect(new URL("/", request.url));
       }
       return NextResponse.next();
     }
+    
     return NextResponse.redirect(new URL("/", request.url));
   } catch (error) {
     console.log(error);
@@ -25,5 +36,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/cart", "/checkout", "/register/singname", "/register/signemail"],
+  matcher: ["/cart", "/checkout", "/register/singname", "/register/signemail", '/order', '/order/orderViewDetail', '/login'],
 };
