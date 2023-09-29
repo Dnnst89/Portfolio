d6eaf7f26a92b519b933a5ab3862017dc6758907
DannyST89 <danny@centaurosolutions.com>
2023-09-29 14:12:34 -0600

    se recibe respuesta de tilopay en thankyou
    cuando el pago es aprovado se muestra un determinado formulario , en caso de error se mostrara
    otro formulario.

--- components/CheckOutForm3.jsx
+++ components/CheckOutForm3.jsx
@@ -1,8 +1,41 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import paymentRequest from "@/api/tilopay/paymentRequest";
+import { useSelector } from "react-redux";
+import Spinner from "./Spinner";
+/*
+  we should store the order number otherwise we cant continue with the
+  payment.
+  order detail is going have three states
+  1- approved = payment successful
+  2- error = insuficient founds
+  3- pendiente = because we should get an order number to continue.
 
-export default function CheckOutForm3({ paymentUrl }) {
-  const router = useRouter();
+  ---- Process to get donde the payment ----
+  step 1 - get the data i need to insert the order
+  - user id - user in sesion
+  - total - any
+  - payment_id - any
+  - state - defualt
+*/
+// workin visa card 5100270000000023
+// insuficient founds card 4112 6134 5159 1116
+export default function CheckOutForm3() {
+  const [paymentUrl, setPaymentUrl] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = await paymentRequest();
+        setPaymentUrl(url);
+      } catch (error) {
+        console.error("Error fetching payment URL:", error);
+        // Handle the error as needed
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="mt-[40px] mx-[30px]">
@@ -14,13 +47,13 @@ export default function CheckOutForm3({ paymentUrl }) {
       </div>
 
       <div className="flex justify-center mt-8 mb-8 w-3/4 ">
-        {/* Redirect to the payment URL */}
-        <button
-          onClick={() => router.push(paymentUrl)}
-          className="bg-pink-200 text-white rounded-sm p-2 w-[200px] whitespace-nowrap"
-        >
-          Proceder al pago
-        </button>
+        {/* Render the payment button when paymentUrl is available */}
+
+        <a href={paymentUrl ?? ""}>
+          <button className="bg-pink-200 text-white rounded-sm p-2 w-[200px] whitespace-nowrap">
+            {paymentUrl ? "Proceder al pago " : <Spinner />}
+          </button>
+        </a>
       </div>
     </div>
   );
