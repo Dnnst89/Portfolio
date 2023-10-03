af35c581a6d9464bf5d28419b56e4c2cd0ebe817
elias-benavides <elias.benavides@centaurosolutions.com>
2023-10-03 15:10:53 -0600

    fix dev

--- components/ProtectedRoutes.jsx
+++ components/ProtectedRoutes.jsx
@@ -5,21 +5,23 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const ProtectedRoutes = ({ children, toLogin = false }) => {
-  const { user } = useSelector((x) => x.auth);
-  const userData = useStorage();
+  const { user } = useStorage();
+  const userData = useSelector((x) => x.auth.user);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
-
   useEffect(() => {
-    if (!user || (!userData?.user?.id && !toLogin)) {
+    console.log(userData);
+    console.log(toLogin);
+    console.log(user?.id);
+    if (!user?.id && !userData?.id && !toLogin) {
       if (window.location.pathname === "/") return;
       return router.push("/");
     }
-    if (user || (userData?.user.id && toLogin)) {
+    if (user?.id && toLogin) {
       return router.push("/");
     }
     setLoading(false);
-  }, [children, router, user, toLogin, userData]);
+  }, [children, router, user, toLogin]);
   return loading ? <></> : children;
 };
 
