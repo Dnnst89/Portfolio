961de093ea9892ca2c70da77ce6b7e36edec8a85
lexie70 <116680625+lexie70@users.noreply.github.com>
2023-10-03 17:43:58 -0600

    estilos ProductDetail

--- components/ProductDetailSecondary.jsx
+++ components/ProductDetailSecondary.jsx
@@ -1,42 +1,54 @@
+"use client";
+import { useState } from "react";
 import ProductReview from "./ProductReview";
 import ProductReviewForm from "./ProductReviewForm";
 
 const ProductDetailSecondary = ({ id, description, reviews }) => {
-
+  const [showAllReviews, setShowAllReviews] = useState(false);
   return (
     <main className="bg-resene" id="detail-table">
-      <section className="flex mx-auto m-5 gap-5 w-6/12 pt-10">
-        <h1 className="m-1 text-xl">Descripción</h1>
-        <p>{description}</p>
+      <section className="flex items-baseline mx-auto gap-5 w-8/12 pt-10">
+        <h1 className="text-xl">Descripción</h1>
+        <p className="w-8/12 text-sm text-grey-100">{description}</p>
       </section>
-      <section className="w-3/4 mt-10 flex mx-auto">
-        <h1 className="text-xl mr-5">Reseñas</h1>
-        <table>
-          <tbody className="tbody-clases">
-            <tr className="border-b-[1px] border-grey flex gap-[170px] w-11/12 ">
-              <th>Nombre de usuario</th>
-              <th>Puntuación</th>
-              <th className="ml-[40px]">Comentario</th>
-            </tr>
-            {reviews
-              ? reviews.map((item) => {
-                  return (
-                    <ProductReview
-                      key={item.id}
-                      comment={item.attributes.comment}
-                      score={item.attributes.score}
-                      user={
-                        item.attributes.users_permissions_user.data.attributes
-                          .username
-                      }
-                    />
-                  );
-                })
-              : null}
-          </tbody>
+      <section className="mt-10 flex justify-center">
+        <h1 className="text-xl mr-2">Reseñas</h1>
+        <table className="w-7/12">
+          <tr className="border-b-2 border-grey-200/50 flex ">
+            <th className="px-4 py-2 w-1/3 text-left whitespace-nowrap ">
+              Nombre de usuario
+            </th>
+            <th className="px-4 py-2 w-1/3 text-left ">Puntuación</th>
+            <th className="px-4 py-2 w-2/3 text-left text-grey-100">
+              Comentario
+            </th>
+          </tr>
+          {reviews
+            ? (showAllReviews ? reviews : reviews.slice(0, 3)).map((item) => {
+                return (
+                  <ProductReview
+                    key={item.id}
+                    comment={item.attributes.comment}
+                    score={item.attributes.score}
+                    user={
+                      item.attributes.users_permissions_user.data.attributes
+                        .username
+                    }
+                  />
+                );
+              })
+            : null}
         </table>
       </section>
-      <ProductReviewForm idProduct={id} />
+      <button
+        className="flex mx-auto pt-2 pb-10  text-lightblue  transition duration-100 opacity-60 hover:opacity-100"
+        onClick={() => setShowAllReviews(!showAllReviews)}
+      >
+        {showAllReviews ? "Ver menos reseñas" : "Ver más reseñas"}
+      </button>
+      <di className="flex justify-center">
+        <ProductReviewForm idProduct={id} />
+      </di>
     </main>
   );
 };
--- components/ProductReviewForm.jsx
+++ components/ProductReviewForm.jsx
@@ -1,28 +1,30 @@
-import { useForm } from "react-hook-form"
-import AddReview from '@/src/graphQl/queries/addReview';
+import { useForm } from "react-hook-form";
+import AddReview from "@/src/graphQl/queries/addReview";
 import { useMutation } from "@apollo/client";
 import toast, { Toaster } from "react-hot-toast";
-import ReCAPTCHA from "react-google-recaptcha"
-import React, { useRef, useState } from 'react';
+import ReCAPTCHA from "react-google-recaptcha";
+import React, { useRef, useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 function ProductReviewForm({ idProduct }) {
-
-  const [rating, setRating] = useState(null)
+  const [rating, setRating] = useState(null);
   const sessionData = JSON.parse(localStorage.getItem("userData"));
-  const captchaRef = useRef(null)
+  const captchaRef = useRef(null);
 
-  const { register, handleSubmit, formState: { errors }, reset } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
   const [createReview] = useMutation(AddReview);
 
   const onSubmit = handleSubmit((data) => {
-
-    const comment = data.comment
-    const score = parseInt(rating)
-    const product = idProduct
-    const users_permissions_user = sessionData.user.id
+    const comment = data.comment;
+    const score = parseInt(rating);
+    const product = idProduct;
+    const users_permissions_user = sessionData.user.id;
     const token = captchaRef.current.getValue();
-
 
     try {
       if (token) {
@@ -31,62 +33,90 @@ function ProductReviewForm({ idProduct }) {
         });
 
         toast.success("Gracias por tu reseña!😍", {
-          autoClose: 5000
+          autoClose: 5000,
         });
-        reset()
-        setRating(0)
+        reset();
+        setRating(0);
         captchaRef.current.reset();
-      }
-      else {
+      } else {
         toast.error("Por favor selecciona la casilla de verificación", {
-          autoClose: 5000
+          autoClose: 5000,
         });
       }
-
+    } catch {
+      toast.error(
+        "Lo sentimos, no se ha podido ingresar la reseña. Intentalo de nuevo más tarde😥",
+        {
+          autoClose: 5000,
+        }
+      );
     }
-    catch {
-      toast.error("Lo sentimos, no se ha podido ingresar la reseña. Intentalo de nuevo más tarde😥", {
-        autoClose: 5000
-      });
-    }
-  })
+  });
 
   return (
     <>
-      {sessionData &&
-
-        < form onSubmit={onSubmit} >
-          <div><Toaster /></div>
+      {sessionData && (
+        <form onSubmit={onSubmit} className="p-5 w-5/12 ">
+          <div>
+            <Toaster />
+          </div>
 
           <div className="text-center">
-            <label>Danos tu opinión del producto</label>
-            <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Escribe aquí tu opinión..."{...register("comment", { required: true })}></textarea>
+            <label htmlFor="message" className="">
+              Danos tu opinión del producto
+            </label>
+            <textarea
+              id="message"
+              rows="4"
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Escribe aquí tu opinión..."
+              {...register("comment", { required: true })}
+            ></textarea>
             {errors.comment && <span>Debes ingresar el comentario</span>}
-            <label>Dale una calificasión</label>
-            <div>
-              {[...Array(5)].map((star, i) => {
-                const ratingValue = i + 1
+            <div className="flex space-x-2">
+              <label>Dale una calificación</label>
+              <div className="flex">
+                {[...Array(5)].map((star, i) => {
+                  const ratingValue = i + 1;
 
-                return (
-                  <label key={i}>
-                    <input type="radio" className="hidden" name="rating" value={ratingValue} onClick={() => setRating(ratingValue)} />
-                    <FaStar className="star" color={ratingValue <= rating ? "#ffc107" : "#ABABAB"} size={20} />
-                  </label>
-                )
-              })}
+                  return (
+                    <label key={i}>
+                      <input
+                        type="radio"
+                        className="hidden"
+                        name="rating"
+                        value={ratingValue}
+                        onClick={() => setRating(ratingValue)}
+                      />
+                      <FaStar
+                        className="star"
+                        color={ratingValue <= rating ? "#ffc107" : "#ABABAB"}
+                        size={20}
+                      />
+                    </label>
+                  );
+                })}
+              </div>
             </div>
-
-            <ReCAPTCHA
-              sitekey="6LfCrUYoAAAAAPgdh0MpvKzzHvhksbGTM3cP1prU"
-              ref={captchaRef}
-            />
-            <button type='submit'>Enviar reseña</button>
+            <div className="flex justify-end">
+              <ReCAPTCHA
+                sitekey="6LfCrUYoAAAAAPgdh0MpvKzzHvhksbGTM3cP1prU"
+                ref={captchaRef}
+              />
+            </div>
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="bg-aquamarine p-2 rounded-md text-white mt-5 mr-10"
+              >
+                Enviar reseña
+              </button>
+            </div>
           </div>
-        </form >
-      }
+        </form>
+      )}
     </>
-
-  )
+  );
 }
 
-export default ProductReviewForm
+export default ProductReviewForm;
