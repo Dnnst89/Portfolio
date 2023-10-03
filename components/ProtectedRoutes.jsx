2194aadba7588a6d1f2a949df6e962aa299b56d8
elias-benavides <elias.benavides@centaurosolutions.com>
2023-10-03 14:48:38 -0600

    save changes

--- components/ProtectedRoutes.jsx
+++ components/ProtectedRoutes.jsx
@@ -1,23 +1,25 @@
 "use client";
+import useStorage from "@/hooks/useStorage";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const ProtectedRoutes = ({ children, toLogin = false }) => {
   const { user } = useSelector((x) => x.auth);
+  const userData = useStorage();
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
-    if (!user && !toLogin) {
+    if (!user || (!userData?.user?.id && !toLogin)) {
       if (window.location.pathname === "/") return;
       return router.push("/");
     }
-    if (user && toLogin) {
+    if (user || (userData?.user.id && toLogin)) {
       return router.push("/");
     }
     setLoading(false);
-  }, [children, router, user, toLogin]);
+  }, [children, router, user, toLogin, userData]);
   return loading ? <></> : children;
 };
 
