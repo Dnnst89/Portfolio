353af51e92f74ec8353624caaa12038c75465506
danii230 <daniela.lopez@centaurosolutions.com>
2023-10-03 13:48:03 -0600

    Fix in filterByAge

--- app/layouts/MainLayout.jsx
+++ app/layouts/MainLayout.jsx
@@ -30,7 +30,7 @@ const MainLayout = ({ children }) => {
         </div>
         <div className="flex justify-center pt-10">
           <GiftIdeasCard
-            ageRange={"1-2years"}
+            ageRange={"1-2"}
             image={heart}
             age={"Menores de 2 años"}
             button={"bg-aquamarine rounded-lg text-white text-sm p-2"}
@@ -39,7 +39,7 @@ const MainLayout = ({ children }) => {
             }
           />
           <GiftIdeasCard
-            ageRange={"2-3years"}
+            ageRange={"2-3"}
             image={wood}
             age={"De 2 a 3 años"}
             button={"bg-lightblue rounded-lg text-white text-sm p-2"}
@@ -48,7 +48,7 @@ const MainLayout = ({ children }) => {
             }
           />
           <GiftIdeasCard
-            ageRange={"4-5years"}
+            ageRange={"4-5"}
             image={brush}
             age={"De 4 a 5 años"}
             button={"bg-yellow-200 rounded-lg text-white text-sm p-2"}
@@ -57,7 +57,7 @@ const MainLayout = ({ children }) => {
             }
           />
           <GiftIdeasCard
-            ageRange={"6-7years"}
+            ageRange={"6-7"}
             image={guitar}
             age={"De 6 a 7 años"}
             button={"bg-pink-200 rounded-lg text-white text-sm p-2"}
@@ -66,7 +66,7 @@ const MainLayout = ({ children }) => {
             }
           />
           <GiftIdeasCard
-            ageRange={"8-9years"}
+            ageRange={"8-9"}
             image={libro}
             age={"De 8 años o mas"}
             button={"bg-yellow-300 rounded-lg text-white text-sm p-2"}
