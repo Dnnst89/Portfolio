7538895b1513836627a851d6dd255f7d97692989
Edgardo Quiros <edgardo@centaurosolutions.com>
2023-10-11 16:39:12 -0600

    Se muestra la informacion en los inputs

--- components/AddressForm.jsx
+++ components/AddressForm.jsx
@@ -1,16 +1,16 @@
 "use client";
 
 import * as Yup from "yup";
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import ErrorForm from "./ErrorForm";
 import { useRouter } from "next/navigation";
 import { useLazyQuery, useMutation, useQuery } from "@apollo/client";
 import { useEffect } from "react";
-import { GET_USER_PAYMENT_INFO } from "@/src/graphQl/queries/getUserPaymentInfo";
 import { CREATE_ADDRESS } from "@/src/graphQl/queries/createAddress";
 import useStorage from "@/hooks/useStorage";
 import InputForm from "./InputForm";
+import { GET_USER_PAYMENT_INFO } from "@/src/graphQl/queries/getUserPaymentInfo";
 const validationSchema = Yup.object().shape({
   firstName: Yup.string()
     .min(2, "Nombre muy corto")
@@ -56,11 +56,10 @@ const validationSchema = Yup.object().shape({
 });
 
 const AddressForm = () => {
-
-  // const { user } = useStorage(); //me trae el usuario autorizado
-  // const userId = user?.id
-  // console.log(userId)
-  const [userInformation, setUserInformation] = useState({ //campos de userAddress
+  const { user } = useStorage(); //me trae el usuario autorizado
+  const id = user?.id;
+  const [userInformation, setUserInformation] = useState({
+    //campos de userAddress
     firstName: "",
     lastName: "",
     email: "",
@@ -76,14 +75,46 @@ const AddressForm = () => {
     idType: "",
   });
 
-  const [createAddress] = useMutation(CREATE_ADDRESS);
+  //const [createAddress] = useMutation(CREATE_ADDRESS);
 
   // const [UpdateUserInformation] = useMutation(UPDATE_USER_INFORMATION);
   // const [UpdateAddress] = useMutation(UPDATE_ADDRES);
   // const [UpdateIdCard] = useMutation(UPDATE_ID_CARD);
+  const { data } = useQuery(GET_USER_PAYMENT_INFO, {
+    variables: { id: id },
+  });
+  useEffect(() => {
+    // Check if data is available and set userInformation
+    if (data && data.usersPermissionsUser) {
+      setUserInformation({
+        firstName: data.usersPermissionsUser.data.attributes.firstName,
+        lastName: data.usersPermissionsUser.data.attributes.lastName,
+        phone: data.usersPermissionsUser.data.attributes.phoneNumber,
 
-
-
+        postCode:
+          data.usersPermissionsUser.data.attributes.users_address.data
+            .attributes.postCode,
+        country:
+          data.usersPermissionsUser.data.attributes.users_address.data
+            .attributes.country,
+        addressLine1:
+          data.usersPermissionsUser.data.attributes.users_address.data
+            .attributes.addressLine1,
+        addressLine2:
+          data.usersPermissionsUser.data.attributes.users_address.data
+            .attributes.addressLine2,
+        province:
+          data.usersPermissionsUser.data.attributes.users_address.data
+            .attributes.province,
+        canton:
+          data.usersPermissionsUser.data.attributes.users_address.data
+            .attributes.canton,
+        idNumber: data.usersPermissionsUser.data.attributes.idCard.idNumber,
+        idType: data.usersPermissionsUser.data.attributes.idCard.idType,
+      });
+    }
+  }, [data]);
+  console.log("user information : ", userInformation);
   const initialValues = {
     firstName: userInformation.firstName,
     lastName: userInformation.lastName,
@@ -98,18 +129,9 @@ const AddressForm = () => {
     checkbox: false,
     idNumber: userInformation.idNumber,
     idType: userInformation.idType,
-
-
   };
-
-  /**
-   * Get
-   */
-
-  const [getUser] = useLazyQuery(GET_USER_PAYMENT_INFO);
-
-
-  const cargar = async () => { //me carga la informacion del usuario si el ya el la ha ingresado
+  const cargar = async () => {
+    //me carga la informacion del usuario si el ya el la ha ingresado
     try {
       const { user } = JSON.parse(localStorage.getItem("userData"));
       if (user) {
@@ -119,23 +141,23 @@ const AddressForm = () => {
         if (data) {
           console.log(data); // Agregar esto para depurar la respuesta
           const userData = data?.usersPermissionsUser?.data?.attributes;
-          const userAddress = data?.usersPermissionsUser?.data?.attributes?.users_address?.data?.attributes;
-          setUserInformation(
-            prevState => ({
-              ...prevState,
-              firstName: userData?.firstName,
-              lastName: userData?.lastName,
-              email: userData?.email,
-              phone: userData?.phoneNumber,
-              postCode: userAddress?.postCode,
-              country: userAddress?.country,
-              addressLine1: userAddress?.addressLine1,
-              addressLine2: userAddress?.addressLine2,
-              province: userAddress?.province,
-              canton: userAddress?.canton,
-              idNumber: userData?.idCard?.idNumber,
-              idType: userData?.idCard?.idType,
-            }));
+          const userAddress =
+            data?.usersPermissionsUser?.data?.attributes?.users_address?.data
+              ?.attributes;
+          setUserInformation({
+            firstName: userData?.firstName,
+            lastName: userData?.lastName,
+            email: userData?.email,
+            phone: userData?.phoneNumber,
+            postCode: userAddress?.postCode,
+            country: userAddress?.country,
+            addressLine1: userAddress?.addressLine1,
+            addressLine2: userAddress?.addressLine2,
+            province: userAddress?.province,
+            canton: userAddress?.canton,
+            idNumber: userData?.idCard?.idNumber,
+            idType: userData?.idCard?.idType,
+          });
         }
       }
     } catch (error) {
@@ -145,9 +167,9 @@ const AddressForm = () => {
 
   useEffect(() => {
     cargar();
-
   }, []);
   console.log(userInformation);
+
   const handleSubmit = (values) => {
     const isoDate = new Date().toISOString();
     const {
@@ -234,9 +256,8 @@ const AddressForm = () => {
   };
   return (
     <Formik
-      initialValues={initialValues}
       validationSchema={validationSchema} // Agrega tu esquema de validación
-      onSubmit={handleSubmit}
+      //onSubmit={handleSubmit}
     >
       {({ errors, touched }) => {
         return (
@@ -250,40 +271,32 @@ const AddressForm = () => {
                       type="text"
                       id="name"
                       name="name"
-                      placeholder="Nombre"
                       className="form-input"
-                    />
-                    <label htmlFor="email">Email</label>
-                    <Field
-                      type="text"
-                      id="email"
-                      name="email"
-                      placeholder="Nombre"
-                      className="form-input"
+                      value={userInformation.firstName}
                     />
                     <label htmlFor="country">Pais</label>
                     <Field
                       type="text"
                       id="country"
                       name="country"
-                      placeholder="País"
                       className="form-input"
+                      value={userInformation.country}
                     />
                     <label htmlFor="canton">Cantón</label>
                     <Field
                       type="text"
                       id="canton"
                       name="canton"
-                      placeholder="Cantón"
                       className="form-input"
+                      value={userInformation.canton}
                     />
                     <label htmlFor="postCode">Código Postal</label>
                     <Field
                       type="text"
                       id="postCode"
                       name="postCode"
-                      placeholder="Código Postal"
                       className="form-input"
+                      value={userInformation.postCode}
                     />
                     <label htmlFor="addressLine2">Direccion 1</label>
                     <Field
@@ -292,6 +305,7 @@ const AddressForm = () => {
                       name="addressLine2"
                       placeholder="Direccion 1"
                       className="form-input"
+                      value={userInformation.addressLine2}
                     />
                   </section>
                   <section className="w-1/4 flex flex-col p-2">
@@ -302,6 +316,7 @@ const AddressForm = () => {
                       name="lastname"
                       placeholder="Apellidos"
                       className="form-input"
+                      value={userInformation.lastName}
                     />
                     <label htmlFor="phone">Teléfono</label>
                     <Field
@@ -310,6 +325,7 @@ const AddressForm = () => {
                       name="phone"
                       placeholder="Teléfono"
                       className="form-input"
+                      value={userInformation.phone}
                     />
                     <label htmlFor="province">Provincia</label>
                     <Field
@@ -318,30 +334,7 @@ const AddressForm = () => {
                       name="province"
                       placeholder="Provincia"
                       className="form-input"
-                    />
-                    <label htmlFor="province">Ciudad</label>
-                    <Field
-                      type="text"
-                      id="province"
-                      name="province"
-                      placeholder="Ciudad"
-                      className="form-input"
-                    />
-                    <label htmlFor="canton">canton</label>
-                    <Field
-                      type="text"
-                      id="canton"
-                      name="canton"
-                      placeholder="canton"
-                      className="form-input"
-                    />
-                    <label htmlFor="addressLine1">Dirección 1</label>
-                    <Field
-                      type="text"
-                      id="addressLine1"
-                      name="addressLine1"
-                      placeholder="direccion 1"
-                      className="form-input"
+                      value={userInformation.province}
                     />
                   </section>
                 </div>
@@ -382,7 +375,6 @@ const AddressForm = () => {
                       htmlFor={"businessid"}
                       id={"businessid"}
                     />
-
                   </section>
                 </div>
               </section>
--- src/graphQl/queries/getUserPaymentInfo.js
+++ src/graphQl/queries/getUserPaymentInfo.js
@@ -1,35 +1,34 @@
 import { gql } from "@apollo/client";
 
 export const GET_USER_PAYMENT_INFO = gql`
-query GetUserAdress ($id: ID!){
-  usersPermissionsUser(id: $id){
-  data{
-    id
-    attributes{
-      firstName
-      lastName
-      phoneNumber
-      email
-      username
-      idCard{
-        idType
-        idNumber
-      }
-      users_address{
-        data{
-          id
-          attributes{
-            postCode
-            country
-            province
-            addressLine1
-            addressLine2
-            canton
+  query GetUserAdress($id: ID!) {
+    usersPermissionsUser(id: $id) {
+      data {
+        id
+        attributes {
+          firstName
+          lastName
+          phoneNumber
+          username
+          idCard {
+            idType
+            idNumber
+          }
+          users_address {
+            data {
+              id
+              attributes {
+                postCode
+                country
+                province
+                addressLine1
+                addressLine2
+                canton
+              }
+            }
           }
         }
       }
     }
   }
-}
-}
 `;
