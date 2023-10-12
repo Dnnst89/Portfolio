d9bb8f9e840e60274a6136ddb27a1605c5f4e498
DannyST89 <danny@centaurosolutions.com>
2023-10-11 20:32:59 -0600

    se modifica el input para que pueda permitir cambios

--- app/checkout/layout.jsx
+++ app/checkout/layout.jsx
@@ -2,11 +2,7 @@ import ProtectedRoutes from "@/components/ProtectedRoutes";
 import React from "react";
 
 const CheckoutLayout = ({ children }) => {
-  return (
-    <>
-      <ProtectedRoutes>{children}</ProtectedRoutes>
-    </>
-  );
+  return <>{children}</>;
 };
 
 export default CheckoutLayout;
