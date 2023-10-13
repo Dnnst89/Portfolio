28cc43ed9011e3c384ca60a530129eb72d2df243
Edgardo Quiros <edgardo@centaurosolutions.com>
2023-10-12 22:38:40 -0600

    fixed querys

--- src/graphQl/queries/updateIdCard.js
+++ src/graphQl/queries/updateIdCard.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const UPDATE_ID_CARD = gql`
-  mutation UPDATE_ID_CARD($id: ID!, $idNumber: Int!, $idType: String!) {
+  mutation updateIdCard($id: ID!, $idNumber: Long!, $idType: String!) {
     updateUsersPermissionsUser(
       id: $id
       data: { idCard: { idNumber: $idNumber, idType: $idType } }
