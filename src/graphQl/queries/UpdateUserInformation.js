926ccec8449962ac56eb0538789d9595acbc9122
DannyST89 <danny@centaurosolutions.com>
2023-10-16 10:34:08 -0600

    se realiza el merge del checkout

--- src/graphQl/queries/UpdateUserInformation.js
+++ src/graphQl/queries/UpdateUserInformation.js
@@ -1,17 +1,19 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const UPDATE_USER_INFORMATION = gql`
-mutation updateUserInformation($firstName: String!,$lastName:String!, $phone: Int!, $email:String, $id: ID! ){
-  updateUsersPermissionsUser(id: $id
-  data:{
-    firstName: $firstName
-    lastName: $lastName
-    phoneNumber: $phone
-    email:$email
-  }
-  )
-  {data: data { id }}
+  mutation updateUserInformation(
+    $firstName: String!
+    $lastName: String!
+    $phone: Int!
+    $id: ID!
+  ) {
+    updateUsersPermissionsUser(
+      id: $id
+      data: { firstName: $firstName, lastName: $lastName, phoneNumber: $phone }
+    ) {
+      data: data {
+        id
+      }
+    }
   }
 `;
-
-
