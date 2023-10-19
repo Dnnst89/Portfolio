d40fe2e54796e39e1364a5559ebed5ac6e742fbd
Fabián Fernández <fabian@centaurosolutions.com>
2023-10-19 10:23:34 -0600

    Update page.jsx

--- app/forgotPassword/page.jsx
+++ app/forgotPassword/page.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useMutation } from "@apollo/client";
+import CheckOutHeader from "@/components/CheckoutHeader";
 import { RESET_PASSWORD } from "../../src/graphQl/queries/resetPassword";
 import { useRouter } from "next/navigation";
 const ResetPasswordSchema = Yup.object().shape({
@@ -40,30 +41,39 @@ const ForgotPassword = () => {
 
   return (
     <div>
-      <h1>Password Reset</h1>
-      <Formik
-        initialValues={{ email: "" }}
-        validationSchema={ResetPasswordSchema}
-        onSubmit={handleSubmit}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <Field type="email" id="email" name="email" />
-              <ErrorMessage name="email" component="div" />
-            </div>
-            <button
-              className="bg-orange text-white rounded-sm"
-              type="submit"
-              disabled={isSubmitting || loading}
-            >
-              Solicitar
-            </button>
-          </Form>
-        )}
-      </Formik>
+
+      <CheckOutHeader regresar={"/personalData"} />
+      <div className="flex flex-col items-center max-w-screen-xl m-auto mt-10">
+
+        <div className="bg-resene  p-5 w-2/4 flex flex-col items-center border-dashed border-2 border-[#787878] drop-shadow-card col-start-3 col-span-8 space-y-5">
+          <h1>Password Reset</h1>
+          <Formik
+            initialValues={{ email: "" }}
+            validationSchema={ResetPasswordSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <div className="space-x-2">
+                  <label htmlFor="email">Email:</label>
+                  <Field type="email" id="email" name="email" />
+                  <ErrorMessage name="email" component="div" />
+                </div>
+                <button
+                  className="bg-aquamarine text-white rounded-sm p-2 flex items-center m-auto mt-5"
+                  type="submit"
+                  disabled={isSubmitting || loading}
+                >
+                  Solicitar
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+
+      </div>
     </div>
+
   );
 };
 
