72b95c19130351d45aa13d5fa20154cc5fc3c9dd
Fabián Fernández <fabian@centaurosolutions.com>
2023-10-30 10:14:56 -0600

    Update page.jsx

--- app/confirmedEmail/page.jsx
+++ app/confirmedEmail/page.jsx
@@ -11,21 +11,21 @@ import { logo } from "../assets/images";
 export default function ThankYouMessage() {
   const router = useRouter();
   return (
-    <div className="bg-floralwhite p-[100px] flex justify-center">
-      <main className="bg-resene border-2 border-dashed border-grey-200 flex flex-col justify-center h-auto p-10">
-        <section className="flex justify-center">
+    <div className="bg-floralwhite flex justify-center max-w-screen-xl m-auto pt-20">
+      <main className="bg-resene border-2 border-dashed border-grey-200 flex flex-col justify-center h-auto p-10 w-full">
+        <section className="justify-center grid md:flex space-y-5 ">
           <figure>
             <Image
               src={logo}
               alt="Detinmarin logo"
-              style={{ width: "390px", height: "170px" }}
+              style={{ width: "auto", height: "170px" }}
             />
           </figure>
           <>
             <div className="flex flex-col items-end justify-center space-y-3">
-              <div className="flex flex-col items-center space-y-1 ml-3">
-                <h1 className="text-xl bold">¡Gracias por registrarte!</h1>
-                <p className="text-sm">
+              <div className="flex flex-col items-center space-y-2 md:ml-3">
+                <h1 className="text-xl bold text-center">¡Gracias por registrarte!</h1>
+                <p className="text-sm text-center">
                   Tu correo se ha confirmado satisfactoriamente{" "}
                 </p>
                 <button
