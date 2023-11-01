6a42635763821606f322e422d3b7141caa39afc0
Fabián Fernández <fabian@centaurosolutions.com>
2023-11-01 09:25:18 -0600

    Update Footer.jsx

--- app/layouts/includes/Footer.jsx
+++ app/layouts/includes/Footer.jsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="bg-resene max-w-screen h-auto p-10 pt-5 mt-10 pb-5">
-      <div className="flex flex-wrap max-w-screen-xl m-auto justify-center items-top">
-        <section className="border-b-2 md:border-r-2 md:border-b-0 border-dashed border-grey-200 pb-4 pl-6 w-full md:w-2/6 grid place-content-center md:h-52 content-baseline">
+      <div className="grid grid-cols-12 max-w-screen-xl m-auto justify-center items-top">
+        <section className="col-span-12 md:col-span-4 py-3 border-dashed border-grey-200 border-b-2 md:border-r-2 md:border-b-0 md:h-64">
           <Image
             src={logo}
             alt="Detinmarin Logo"
@@ -20,61 +20,75 @@ const Footer = () => {
           </p>
         </section>
 
-        <section className="border-b-2 md:border-r-2 md:border-b-0 border-dashed border-grey-200 pb-4 pl-6 w-full md:w-2/6 grid place-content-center md:h-52 content-baseline">
-          <div>
-            <h1 className="text-lg relative right-5 top-4">Contacto</h1>
+        <section className="col-span-12 md:col-span-4 py-3 border-dashed border-grey-200 border-b-2 md:border-r-2 md:border-b-0 md:h-64">
+          <div className="md:ml-5">
+            <h1 className="text-lg">Contacto</h1>
           </div>
-          <ul className="ml-1 text-sm list-disc space-y-1 mt-5 ">
-            <li className="w-auto">
-              Costa Rica, San Jose, Escazu, San Rafael,{<br />} Plaza Maynar Local
-              #20
-            </li>
-            <li>
-              Correo: <span className="underline">hola@detinmarin.cr </span>
-            </li>
-            <li>
-              Teléfono: <span className="underline">(506) 8771-6588</span>
-            </li>
-            <li>
-              Horario: Lunes a Sabado de 10am-7pm {<br />}y domingos de 11am-5pm
-            </li>
-          </ul>
-          
+          <div className="ml-5 md:ml-10 text-sm">
+            <ul className="list-disc space-y-2">
+              <li className="w-auto">
+                Costa Rica, San Jose, Escazu, San Rafael,{<br />} Plaza Maynar Local
+                #20
+              </li>
+              <li>
+                Correo: <Link
+                  href={"mailto:hola@detinmarin.cr"}
+                  className="hover:underline"
+                >
+                  <span className="underline">hola@detinmarin.cr</span>
+                </Link>
+              </li>
+              <li>
+                Correo: <Link
+                  href={"tel:+506-8771-6588"}
+                  className="hover:underline"
+                >
+                  <span className="underline">(506) 8771-6588</span>
+                </Link>
+              </li>
+              <li>
+                Horario: Lunes a Sabado de 10am-7pm {<br />}y domingos de 11am-5pm
+              </li>
+            </ul>
+          </div>
         </section>
 
-        <section className="border-b-2 md:border-r-2 md:border-b-0 border-dashed border-grey-200 pb-4 pl-6 w-full md:w-1/6 grid place-content-center md:h-52 content-baseline">
-          <div>
-            <h1 className="text-lg relative md:right-5 top-4">Productos</h1>
+        <section className="col-span-12 md:col-span-2 py-3 border-dashed border-grey-200 border-b-2 md:border-r-2 md:border-b-0 md:h-64">
+          <div className="md:ml-5">
+            <h1 className="text-lg">Productos</h1>
           </div>
-
-          <ul className="text-sm list-disc p-6 space-y-1">
-            <li>Categorías</li>
-            <li>Edades </li>
-            <li>Marcas </li>
-          </ul>
+          <div className="ml-5 md:ml-10 text-sm">
+            <ul className="list-disc space-y-2">
+              <li>Categorías</li>
+              <li>Edades </li>
+              <li>Marcas </li>
+            </ul>
+          </div>
         </section>
 
-        <section className="w-full md:w-1/6 grid place-content-center pl-6 md:h-52 content-baseline">
-          <div>
-            <h1 className="text-lg relative md:right-5 top-4">Redes sociales</h1>
+        <section className="col-span-12 md:col-span-2 py-3 md:h-64">
+          <div className="md:ml-5">
+            <h1 className="text-lg">Redes sociales</h1>
           </div>
-          <ul className="text-sm list-disc p-6 flex flex-col space-y-1">
-            <Link
-              href={"https://www.facebook.com/detinmarin.cr7"}
-              className="hover:underline space-y-5 "
-            >
-              <li>Facebook</li>
-            </Link>
-            <Link
-              href={"https://www.instagram.com/detinmarin.cr/"}
-              className="hover:underline"
-            >
-              <li>Instagram</li>
-            </Link>
-            <Link href={""} className="hover:underline">
-              <li>WhatsApp</li>
-            </Link>
-          </ul>
+          <div className="ml-5 md:ml-10 text-sm">
+            <ul className="list-disc grid space-y-2">
+              <Link
+                href={"https://www.facebook.com/detinmarin.cr7"}
+                className="hover:underline space-y-5 "
+              >
+                <li>Facebook</li>
+              </Link>
+              <Link
+                href={"https://www.instagram.com/detinmarin.cr/"}
+                className="hover:underline"
+              >
+                <li>Instagram</li>
+              </Link>
+              <Link href={"https://wa.me/50687716588"} className="hover:underline">
+                <li>WhatsApp</li>
+              </Link>
+            </ul>
+          </div>
         </section>
 
       </div>
