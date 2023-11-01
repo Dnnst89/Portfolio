c0c4f13a37beae6b61100e4602a377c1b2a19d33
danii230 <daniela.lopez@centaurosolutions.com>
2023-11-01 16:03:22 -0600

    Fix

--- components/GiftIdeasCard.jsx
+++ components/GiftIdeasCard.jsx
@@ -8,22 +8,24 @@ function GiftIdeasCard({ ageRange, image, age, button, border }) {
   const router = useRouter();
   return (
     //  <Link href={{ pathname: "/resultsByAge", query: { ageRange } }}>
-    <div className=" m-4">
-      <section className="mt-5 ">
-        <div className={border}>
-          <Image
-            src={image}
-            alt="heart image"
-            className="rounded-[10px] w-[140px] md:w-[220px]"
-          />
-          <div className="flex justify-center m-auto mt-5">
-            <h1 className="text-xs md:text-xl ">{age}</h1>
+    <div onClick={() => { return router.push(`/resultsByAge/?query=${ageRange}`) }}>
+      <div className=" m-4">
+        <section className="mt-5 ">
+          <div className={border}>
+            <Image
+              src={image}
+              alt="heart image"
+              className="rounded-[10px] w-[140px] md:w-[220px]"
+            />
+            <div className="flex justify-center m-auto mt-5">
+              <h1 className="text-xs md:text-xl ">{age}</h1>
+            </div>
+            <div className="flex justify-center m-auto mt-5">
+              <button className={button}> A EXPLORAR</button>
+            </div>
           </div>
-          <div className="flex justify-center m-auto mt-5">
-            <button onClick={() => { return router.push(`/resultsByAge/?query=${ageRange}`) }} className={button}> A EXPLORAR</button>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
     // </Link>
   );
