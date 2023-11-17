0562052db49d9507b15436562d9fcea6ed63c310
Fabián Fernández <fabian@centaurosolutions.com>
2023-11-17 10:30:52 -0600

    Front ProductFeatureSelector

--- components/AddItemBtn.jsx
+++ components/AddItemBtn.jsx
@@ -12,18 +12,18 @@ import UPDATE_CART_ITEM_QUANTITY_MUTATION from "@/src/graphQl/queries/updateCart
 const AddItemBtn = ({ quantityItem, variant, cartItems, cartQuantity, sessionId, user, features, enableButton }) => {
 
 
+
     const dispatch = useDispatch();
     const { isAuthenticated } = useSelector((x) => x.auth);
     const [createCartItem] = useMutation(CREATE_CART_ITEM_MUTATION, {});
     const [updateCartItemQuantity] = useMutation(UPDATE_CART_ITEM_QUANTITY_MUTATION);
     const handleAdd = () => {
+       
         //filtro los items para verificar si ya ese producto fue agregado, si fue agregado, se actualiza los  item en el carrito
         //si no esta en los items del carrito se crea o agrega como uno nuevo
         if (!user?.id && !isAuthenticated) {
             toast.error("Debe iniciar sesión para agregar este producto al carrito")
         } else {
-
-
             const itemFiltrado = cartItems.find((item) => item.attributes.variant.data.id === variant?.id);
             const fechaActual = new Date();
             const fechaFormateada = fechaActual.toISOString();
@@ -41,17 +41,13 @@ const AddItemBtn = ({ quantityItem, variant, cartItems, cartQuantity, sessionId,
                         .then((response) => {
                             dispatch(updateQtyItems(cartQuantity + quantityItem))
                             toast.success("Se ha actulizado un producto");
-
                             // Manejar la respuesta de la mutación aquí, si es necesario
-
                         })
                         .catch((error) => {
                             // Manejar errores de la mutación aquí
                             toast.error('Ha sucedido un error ');
                         });
                 }
-
-
             } else {//si el item nunca se ha agregado al carrito
                 if (variant?.attributes?.stock > 0) {//si el stock del item es 0
                     createCartItem({ //se crea un nuevo item en el carrito
@@ -79,14 +75,10 @@ const AddItemBtn = ({ quantityItem, variant, cartItems, cartQuantity, sessionId,
                 }
             }
         }
-
-
-
     };
     return (
         <div>
             {" "}
-
             <button disabled={!enableButton} className="text-white text-sm" onClick={handleAdd}>
                 Agregar al carrito
             </button>
