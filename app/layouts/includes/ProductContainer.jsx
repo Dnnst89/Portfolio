2a2692c2b03c7fb1ed33d1754ea6a6170bdd07b9
danii230 <daniela.lopez@centaurosolutions.com>
2023-12-04 13:23:54 -0600

    Fix algolia search
    se agrega la funcionalidad del enter en las busquedas

--- app/layouts/includes/ProductContainer.jsx
+++ app/layouts/includes/ProductContainer.jsx
@@ -19,31 +19,33 @@ const ProductContainer = ({
   }, [currentPage]);
 
   return (
-    <>
-      <div className="flex flex-wrap max-w-screen-xl m-auto justify-center">
-        {hits
-          ? hits.map((item) => {
-            return (
-              <ProductCard
-                key={item.id}
-                id={item.id}
-                name={item.name}
-                defaultPrice={item.defaultPrice}
-                coverImage={item.coverImage}
-                brand={item.brand}
-              />
-            );
-          })
-          : null}
+    <> {nbHits > 0 ? (
+      <div>
+        <div className="flex flex-wrap max-w-screen-xl m-auto justify-center">
+          {hits
+            ? hits.map((item) => {
+              return (
+                <ProductCard
+                  key={item.id}
+                  id={item.id}
+                  name={item.name}
+                  defaultPrice={item.defaultPrice}
+                  coverImage={item.coverImage}
+                  brand={item.brand}
+                />
+              );
+            })
+            : null}
+        </div>
+        <Pagination
+          hitsPerPage={hitsPerPage}
+          nbHits={nbHits}
+          nbPages={nbPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
-      <Pagination
-        hitsPerPage={hitsPerPage}
-        nbHits={nbHits}
-        nbPages={nbPages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-    </>
+    ) : (<div className="text-center grid content-center h-80 m-auto"> <h1 className="font-bold">¡Lo sentimos!</h1> <h2>No se encontraron resultados.</h2> </div>)} </>
   );
 };
 export default ProductContainer;
