1927fea01add351c3ec222d1cf53b21af206d712
danii230 <daniela.lopez@centaurosolutions.com>
2023-12-07 13:24:03 -0600

    Fix algolia search
    Se le agrega un spinner al mostrar resultados

--- components/ResultsComponent.jsx
+++ components/ResultsComponent.jsx
@@ -3,6 +3,7 @@ import ProductContainer from "@/app/layouts/includes/ProductContainer";
 import { algoliaInstace } from "@/src/axios/algoliaIntance/config";
 import React, { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import Spinner from "@/components/Spinner";
 
 const ResultsComponent = (test) => {
 
@@ -11,6 +12,7 @@ const ResultsComponent = (test) => {
   const [nbHits, setNbHits] = useState(null);
   const [nbPages, setNbPages] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     console.log(test);
@@ -34,38 +36,54 @@ const ResultsComponent = (test) => {
   }
 
   async function allResults() {
-    const result = await getHits();
-    const { hitsPerPage, nbHits, nbPages } = result;
-    setResult(result);
-    setHitsPerPage(hitsPerPage);
-    setNbHits(nbHits);
-    setNbPages(nbPages);
+    try {
+      setLoading(true); // Establece loading en true antes de realizar la solicitud
+      const result = await getHits();
+      const { hitsPerPage, nbHits, nbPages } = result;
+      setResult(result);
+      setHitsPerPage(hitsPerPage);
+      setNbHits(nbHits);
+      setNbPages(nbPages);
+    } catch (err) {
+      // Manejar errores si es necesario
+    } finally {
+      setLoading(false); // Establece loading en false independientemente del éxito o fallo
+    }
   }
 
   useEffect(() => {
     if (test.query) {
       allResults();
+      // setLoading(false)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentPage, test.query]);
 
+
   return (
-    <> {nbHits > 0 ? (
-      <div>
-        <div className="flex flex-wrap max-w-screen-xl m-auto justify-center my-10">
-          <h1>Resultados de &#34;{decodeURIComponent(test.query)}&#34;</h1>
-        </div>
-        <Toaster />
-        <ProductContainer
-          result={result}
-          hitsPerPage={hitsPerPage}
-          nbHits={nbHits}
-          nbPages={nbPages}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
+    <>
+      <div className={loading ? "grid place-items-center" : ""}>
+        {loading ? <Spinner /> :
+
+          nbHits > 0 ? (
+            <div>
+              <div className="flex flex-wrap max-w-screen-xl m-auto justify-center my-10">
+                <h1>Resultados de &#34;{decodeURIComponent(test.query)}&#34;</h1>
+              </div>
+              <Toaster />
+              <ProductContainer
+                result={result}
+                hitsPerPage={hitsPerPage}
+                nbHits={nbHits}
+                nbPages={nbPages}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+              />
+            </div>
+          ) : (<div className="text-center grid content-center h-80 m-auto"> <h1 className="font-bold">¡Lo sentimos!</h1> <h2>No se encontraron resultados.</h2> </div>)
+        }
       </div>
-    ) : (<div className="text-center grid content-center h-80 m-auto"> <h1 className="font-bold">¡Lo sentimos!</h1> <h2>No se encontraron resultados.</h2> </div>)} </>
+    </>
   );
 };
 
