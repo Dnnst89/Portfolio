2d067e86a555093639621fc94d6cd8ca8358c861
Edgardo Quiros <edgardo@centaurosolutions.com>
2023-12-21 10:55:04 -0600

    acomodar para movil

--- app/layouts/includes/ProductContainer.jsx
+++ app/layouts/includes/ProductContainer.jsx
@@ -20,7 +20,7 @@ const ProductContainer = ({
 
   return (
     <div>
-      <div className="flex flex-wrap  items-center w-full m-auto md:p-4 sm:p-2">
+      <div className="flex flex-wrap items-center justify-center w-full m-auto sm:p-4">
         {hits
           ? hits.map((item) => {
               return (
