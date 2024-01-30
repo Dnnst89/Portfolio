9f9203dcb1342561cb8bf2bf556db4007b3de1f3
DESKTOP-CD12KCE\Daniel Rojas <daniel.rojas@centaurosolutions.com>
2024-01-30 11:31:22 -0600

    Inclusión de Google Analytics
    Se incluye la funcionalidad de Google Analytics y su ID de medición en la variables del .env.

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Poppins } from "next/font/google";
 import ApolloProviders from "@/src/graphQl/ApolloProviders";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 require("dotenv").config();
 
@@ -20,13 +21,15 @@ const poppins = Poppins({
 });
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${poppins.className} bg-floralwhite`}>
-        <ApolloProviders>
-          <Providers>{children}</Providers>
-        </ApolloProviders>
-      </body>
-    </html>
+  return (    
+      <html lang="en">
+        <body className={`${poppins.className} bg-floralwhite`}>
+          <ApolloProviders>
+            <Providers>{children}</Providers>
+          </ApolloProviders>
+        </body>
+        {/* Adding GOOGLE ANALYTICS for all routes in the app. */}
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}/>
+      </html>
   );
 }
