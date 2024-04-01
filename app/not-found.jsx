24ac4a02a3a12c88bc77c051dde8b4d89b0a7e0d
Edgardo Quiros <edgardo@centaurosolutions.com>
2024-04-01 09:56:05 -0600

    eliminar proteccion route

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,21 +3,18 @@ import Link from "next/link";
 import { BiArrowBack } from "react-icons/bi";
 
 const NotFound = () => {
-  try {
-    useProtectionRoute();
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="text-center m-5 flex flex-col justify-center items-center">
-          <p>Lo sentimos, ha ocurrido un problema</p>
-          <Link href={"/"} className="p-2">
-            <button className="bg-aquamarine text-white rounded-sm p-2 flex items-center">
-              Ir a inicio
-            </button>
-          </Link>
-        </div>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center m-5 flex flex-col justify-center items-center">
+        <p>Lo sentimos, ha ocurrido un problema</p>
+        <Link href={"/"} className="p-2">
+          <button className="bg-aquamarine text-white rounded-sm p-2 flex items-center">
+            Ir a inicio
+          </button>
+        </Link>
       </div>
-    );
-  } catch (error) {}
+    </div>
+  );
 };
 
 export default NotFound;
