e7e02a8dac94c9aebf570cdd06e360f6445dd102
Edgardo Quiros <edgardo@centaurosolutions.com>
2024-04-01 09:48:56 -0600

    agregar rutas protegidas a la pagina not found

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,18 +3,21 @@ import Link from "next/link";
 import { BiArrowBack } from "react-icons/bi";
 
 const NotFound = () => {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="text-center m-5 flex flex-col justify-center items-center">
-        <p>Lo sentimos, ha ocurrido un problema</p>
-        <Link href={"/"} className="p-2">
-          <button className="bg-aquamarine text-white rounded-sm p-2 flex items-center">
-            Ir a inicio
-          </button>
-        </Link>
+  try {
+    useProtectionRoute();
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="text-center m-5 flex flex-col justify-center items-center">
+          <p>Lo sentimos, ha ocurrido un problema</p>
+          <Link href={"/"} className="p-2">
+            <button className="bg-aquamarine text-white rounded-sm p-2 flex items-center">
+              Ir a inicio
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {}
 };
 
 export default NotFound;
