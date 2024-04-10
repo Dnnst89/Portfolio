9fef22da1b406cf6b0bf75411a79f0543b27e61e
DESKTOP-RBFVB42\Zaida <zaida.gonzalez@centaurosolutions.com>
2024-04-10 11:34:25 -0600

    fix local storage

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -10,7 +10,9 @@ import "../../styles/fonts.css";
 
 export default function CartView() {
 
+  if(navigatedFromOrderComponent){
   localStorage.removeItem("navigatedFromOrderComponent");
+  }
   useProtectionRoute()
   return (
     <div className="bg-floralwhite flex flex-wrap max-w-screen-xl m-auto justify-center">
