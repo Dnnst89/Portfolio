edd4ebec5dfba6a8cc3a58656201df310d425c1f
dnnst89 <dsdevelopercr@outlook.com>
2024-06-02 22:41:48 -0600

    added contact me

--- src/components/contactMe/ContactMe.tsx
+++ src/components/contactMe/ContactMe.tsx
@@ -1,11 +1,14 @@
 import React, { FormEvent, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { FaEnvelope } from "react-icons/fa";
 
 const ContactMe = () => {
   const form = useRef<HTMLFormElement>(null);
   const [formData, setFormData] = useState({
+    devName: "",
     user_name: "",
     user_email: "",
+    subject: "", // Added subject field
     message: "",
   });
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
@@ -19,6 +22,10 @@ const ContactMe = () => {
       newErrors.user_email = "Email is required";
     } else if (!/^\S+@\S+\.\S+$/.test(formData.user_email)) {
       newErrors.user_email = "Email is invalid";
+    }
+    if (!formData.subject.trim()) {
+      // Validate subject field
+      newErrors.subject = "Subject is required";
     }
     if (!formData.message.trim()) {
       newErrors.message = "Message is required";
@@ -44,11 +51,18 @@ const ContactMe = () => {
           process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY ?? ""
         )
         .then(
-          () => {
-            console.log("SUCCESS!");
+          (response) => {
+            console.log("Email sent successfully:", response);
+            setFormData({
+              devName: "Danny Soto J.",
+              user_name: "",
+              user_email: "",
+              subject: "",
+              message: "",
+            });
           },
           (error) => {
-            console.log("FAILED...", error.text);
+            console.error("Failed to send email:", error);
           }
         );
     }
@@ -59,9 +73,10 @@ const ContactMe = () => {
       onSubmit={sendEmail}
       className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-lg"
     >
-      <h2 className="text-2xl font-bold mb-6 text-center text-[#0891b2]">
-        Contact Us
-      </h2>
+      <div className="flex items-center mb-6">
+        <FaEnvelope className="text-[#0891b2] mr-2" size={50} />
+        <h2 className="text-2xl font-bold text-[#0891b2]">Contact Us</h2>
+      </div>
       <div className="mb-4">
         <label
           htmlFor="user_name"
@@ -85,7 +100,7 @@ const ContactMe = () => {
       <div className="mb-4">
         <label
           htmlFor="user_email"
-          className="block text-gray-700 text-sm font-bold mb-2"
+          className=" text-gray-700 text-sm font-bold mb-2 flex items-center"
         >
           Email
         </label>
@@ -100,6 +115,28 @@ const ContactMe = () => {
         />
         {errors.user_email && (
           <span className="text-red-500 text-sm">{errors.user_email}</span>
+        )}
+      </div>
+      <div className="mb-4">
+        {" "}
+        {/* Added subject field */}
+        <label
+          htmlFor="subject"
+          className="block text-gray-700 text-sm font-bold mb-2"
+        >
+          Subject
+        </label>
+        <input
+          type="text"
+          name="subject"
+          value={formData.subject}
+          onChange={handleChange}
+          className={`w-full px-3 py-2 border ${
+            errors.subject ? "border-red-500" : "border-gray-300"
+          } rounded-md shadow-sm focus:outline-none focus:ring focus:border-[#0891b2]`}
+        />
+        {errors.subject && (
+          <span className="text-red-500 text-sm">{errors.subject}</span>
         )}
       </div>
       <div className="mb-6">
